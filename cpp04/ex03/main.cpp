2105e47faa7d468394b926fa7e14baf668d58979
Nassim Achab <nachab@c1r2p2.1337.ma>
2023-10-27 13:24:28 +0100

    changed the main

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -3,16 +3,24 @@
 #include "include/Cure.hpp"
 #include "include/ICharacter.hpp"
 #include "include/Character.hpp"
+#include "include/MateriaSource.hpp"
 
-int main() {
-    AMateria    *ice = new Ice();
-    ICharacter  *rob = new Character("rob");
-    rob->equip(ice);
-    rob->equip(new Ice());
-    rob->equip(new Ice());
-    rob->equip(new Cure());
-    rob->equip(new Ice());
-    
-    rob->use(3, *rob);
-    delete ice;
+int main()
+{
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+    ICharacter* me = new Character("me");
+    AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+    ICharacter* bob = new Character("bob");
+    me->use(0, *bob);
+    me->use(1, *bob);
+    delete bob;
+    delete me;
+    delete src;
+    return 0;
 }
