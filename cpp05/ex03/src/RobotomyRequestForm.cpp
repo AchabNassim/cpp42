a87c16ad4d46e70aea27ef4cdf5045052d5c5ef0
AchabNassim <achab.nassim.solicode@gmail.com>
2023-12-01 10:57:11 +0100

    Added the final ex of cpp05

---
+++ cpp05/ex03/src/RobotomyRequestForm.cpp
@@ -0,0 +1,28 @@
+#include "../inc/RobotomyRequestForm.hpp"
+#include "../inc/Bureaucrat.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm() : AForm("RobotomyRequestForm", 72, 45) {
+}
+
+RobotomyRequestForm::RobotomyRequestForm(std::string name) : AForm(name, 72, 45) {
+}
+
+RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm& ref) : AForm(ref.getName(), 72, 45) {
+}
+
+RobotomyRequestForm::~RobotomyRequestForm() {
+}
+
+void    RobotomyRequestForm::execute(Bureaucrat const &bureaucrat) const {
+    if (this->getIsSigned() && this->getExecGrade() >= bureaucrat.getGrade()) {
+        static int successRate;
+        if (successRate & 1)
+            std::cout << "Robotomy failed :)\n";
+        else
+            std::cout << this->getName() << " had a successfull Robotomy :(\n";
+        successRate++;
+    }
+    else {
+        throw Bureaucrat::GradeTooLowException();
+    }
+}
---
+++ cpp05/ex03/src/ShrubberyCreationForm.cpp
@@ -0,0 +1,34 @@
+#include "../inc/ShrubberyCreationForm.hpp"
+#include "../inc/Bureaucrat.hpp"
+#include <fstream>
+
+ShrubberyCreationForm::ShrubberyCreationForm() : AForm("ShrubberyCreationForm", 145, 137) {
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string name) : AForm(name, 145, 137) {
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm& ref) : AForm(ref.getName(), 145, 137) {
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm() {
+}
+
+void    ShrubberyCreationForm::execute(Bureaucrat const &bureaucrat) const {
+    if (this->getIsSigned() && this->getExecGrade() >= bureaucrat.getGrade()) {
+        std::ofstream file(this->getName().c_str());
+        file << "        ^\n"
+                "       ^^^\n"
+                "      ^^^^^\n"
+                "     ^^^^^^^\n"
+                "    ^^^^^^^^^\n"
+                "   ^^^^^^^^^^^\n"
+                "  ^^^^^^^^^^^^^\n"
+                " ^^^^^^^^^^^^^^^\n"
+                "^^^^^^^^^^^^^^^^^\n"
+                "       |||\n";
+    }
+    else {
+        throw Bureaucrat::GradeTooLowException();
+    }
+}
