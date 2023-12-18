2500591218833fb6250bede2f4b973e7996443fd
AchabNassim <achab.nassim.solicode@gmail.com>
2023-12-18 18:50:12 +0100

    cpp07 ex01 more test cases

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -3,10 +3,10 @@
 #include <iostream>
 
 template <typename T>
-void    iter(T *address, T length, void (*operation)(T &elem)) {
+void    iter(T *address, size_t length, void (*operation)(T &elem)) {
     if (!address)
         return ;
-    for (int i = 0; i < length; i++) {
+    for (size_t i = 0; i < length; i++) {
         operation(address[i]);
     }
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,12 +1,16 @@
 #include "iter.hpp"
 
 template <typename T>
-void    change(T &elem) {
-    std::cout << elem << std::endl;
+void    print(T &elem) {
+    std::cout << elem << " " << typeid(elem).name() << std::endl;
 }
 
 int main() {
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    iter(arr, 10, change);
+    int intArr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    float floatArr[5] = {1.5, 1.6, 1.7, 1.8, 1.9};
+    std::string strArr[3] = {"hey", "it's", "working"};
+    iter(intArr, 10, print);
+    iter(floatArr, 5, print);
+    iter(strArr, 3, print);
     return (0);
 }
