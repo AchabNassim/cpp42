8a565a980704f7f0167144d29f364db63d67b4ee
AchabNassim <achab.nassim.solicode@gmail.com>
2023-12-21 18:31:57 +0000

    .

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -9,8 +9,8 @@ int main() {
     int intArr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     float floatArr[5] = {1.5, 1.6, 1.7, 1.8, 1.9};
     std::string strArr[3] = {"hey", "it's", "working"};
-    iter(intArr, 10, print);
-    iter(floatArr, 5, print);
-    iter(strArr, 3, print);
+    iter(intArr, 10, print<int>);
+    iter(floatArr, 5, print<const float>);
+    iter(strArr, 3, print<const std::string>);
     return (0);
 }
