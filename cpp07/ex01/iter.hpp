d2ccc1e5e1ad2ee21a4d9b56d31b332df6f64c42
AchabNassim <achab.nassim.solicode@gmail.com>
2023-12-21 18:31:07 +0000

    template cpp07 ex01 supports two types instead of one

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
 #include <iostream>
-
-template <typename T>
-void    iter(T *address, size_t length, void (*operation)(T &elem)) {
+template <typename T, typename K>
+void    iter(T *address, size_t length, void (*operation)(K &elem)) {
     if (!address)
         return ;
     for (size_t i = 0; i < length; i++) {
