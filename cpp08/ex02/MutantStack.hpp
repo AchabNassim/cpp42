f43c8d4084b2c1c6d3fe98b4b58350fc6144b67c
AchabNassim <achab.nassim.solicode@gmail.com>
2023-12-31 14:05:31 +0000

    cpp08/ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -7,6 +7,15 @@
 template <typename T, class Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
     public :
+        MutantStack() {};
+        MutantStack(const MutantStack& ref) : std::stack<T, Container>(ref) {}
+        MutantStack& operator=(const MutantStack& ref) {
+            if (this == &ref)
+                return (*this);
+            this->c = ref.c;
+            return (*this);
+        }
+        ~MutantStack() {}
         typedef typename Container::iterator iterator;
         const iterator begin() const {
             return (this->c.begin());
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -2,19 +2,19 @@
 
 int main()
 {
-    std::list<int> mstack;
-    mstack.push_back(5);
-    mstack.push_back(17);
-    std::cout << mstack.back() << std::endl;
-    mstack.pop_back();
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
     std::cout << mstack.size() << std::endl;
-    mstack.push_back(3);
-    mstack.push_back(5);
-    mstack.push_back(737);
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
     //[...]
-    mstack.push_back(0);
-    std::list<int>::iterator it = mstack.begin();
-    std::list<int>::iterator ite = mstack.end();
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
     ++it;
     --it;
     while (it != ite)
@@ -22,6 +22,6 @@ int main()
     std::cout << *it << std::endl;
     ++it;
     }
-    std::stack<int, std::list<int> > s(mstack);
+    std::stack<int> s(mstack);
     return 0;
 }
