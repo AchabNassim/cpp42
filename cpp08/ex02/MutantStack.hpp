19b97f20d00d3a1c5f4f73e97d3adf4529b21a22
AchabNassim <achab.nassim.solicode@gmail.com>
2023-12-31 13:30:02 +0000

    cpp08/ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <stack>
+#include <vector>
+#include <deque>
+#include <list>
 
-template <typename T>
-class MutantStack {
+template <typename T, class Container = std::deque<T>>
+class MutantStack : public std::stack<T, Container> {
     public :
-    
-
+        typedef typename Container::iterator iterator;
+        const iterator begin() const {
+            return (this->c.begin());
+        }
+        const iterator end() const {
+            return (this->c.end());
+        }
+        iterator begin() {
+            return (this->c.begin());
+        }
+        iterator end() {
+            return (this->c.end());
+        }
 };
