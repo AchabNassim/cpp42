561d9fed9e834c052f4cf986027dbcb9aec9ff01
Nassim <achab.nassim.solicode@gmail.com>
2024-01-25 13:45:37 +0100

    added jacobsthal sequence in cpp09

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -46,7 +46,7 @@ void sortPair(std::vector<int> &pair) {
 
 bool comparefirstElement(std::vector<int> &first, std::vector<int> &second) {
     comparisons++;
-    return (first[0] < second[0]);
+    return (first[1] < second[1]);
 }
 
 bool lowerBoundCompare(int a, int b) {
@@ -69,26 +69,60 @@ void makePairs(std::vector<std::vector<int> > &pairs, std::vector<int> &array) {
 void    fillChains(std::vector<std::vector<int> > &pairs,std::vector<int> &main, std::vector<int> &pend) {
     for (int i = 0; i < pairs.size(); i++) {
         if (pairs[i].size() == 2) {
-            main.push_back(pairs[i][0]);
-            pend.push_back(pairs[i][1]);
-        } else {
+            main.push_back(pairs[i][1]);
             pend.push_back(pairs[i][0]);
         }
     }
 }
 
+std::vector<int> generateJacobsthalSequence(int n) {
+    std::vector<int> sequence;
+    int a = 0, b = 1;
+    for (int i = 0; i < n; ++i) {
+        sequence.push_back(a);
+        int temp = a;
+        a = b;
+        b = a + 2 * temp;
+    }
+    return sequence;
+}
+
+std::vector<int> buildJacobsthalSequence(int length) {
+    std::vector<int> sequence;
+    sequence.push_back(0);
+    sequence.push_back(1);
+
+    for (int i = 2; i <= length; ++i) {
+        sequence.push_back(sequence[i - 1] + 2 * sequence[i - 2]);
+    }
+
+    return sequence;
+}
+
 // Function to implement the Merge Insertion Sort algorithm
 void mergeInsertionSort(std::vector<int>& vec) {
+    int straggler = -1;
     std::vector<std::vector<int> > pairs;
     std::vector<int> main;
     std::vector<int> pend;
     
+    if (vec.size() % 2 != 0) {
+        straggler = vec.back();
+        vec.pop_back();
+    }
     makePairs(pairs, vec);
     std::sort(pairs.begin(), pairs.end(), comparefirstElement);
     fillChains(pairs, main, pend);
+    std::vector<int> jacobsthalSeq = buildJacobsthalSequence(pend.size());
     for (int i = 0; i < pend.size(); i++) {
-        main.insert(std::lower_bound(main.begin(), main.end(), pend[i], lowerBoundCompare), pend[i]);
+        int insertionIndex = jacobsthalSeq[i];
+        if (jacobsthalSeq[i] < pend.size())
+            main.insert(std::lower_bound(main.begin(), main.end(), pend[insertionIndex], lowerBoundCompare), pend[insertionIndex]);
+        else
+            main.insert(std::lower_bound(main.begin(), main.end(), pend[i], lowerBoundCompare), pend[i]);
     }
+    if (straggler != -1)
+        main.insert(std::lower_bound(main.begin(), main.end(), straggler, lowerBoundCompare), straggler);
     std::cout << main << std::endl;
 }
 
